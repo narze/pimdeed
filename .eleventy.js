9f9e6c35558d8e38ad53a0211b43d4ab00e04807
Manassarn Manoonchai <manassarn@gmail.com>
2020-07-14 18:49:32 +0700

    :heavy_plus_sign: Add AlpineJS

--- .eleventy.js
+++ .eleventy.js
@@ -5,6 +5,10 @@ module.exports = function (eleventyConfig) {
 
   eleventyConfig.addPassthroughCopy({ "./_tmp/style.css": "./style.css" });
 
+  eleventyConfig.addPassthroughCopy({
+    "./node_modules/alpinejs/dist/alpine.js": "./js/alpine.js",
+  });
+
   // eleventyConfig.addShortcode("version", function () {
   //   return String(Date.now());
   // });
